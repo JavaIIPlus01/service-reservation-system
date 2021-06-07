b48bf4f9668be854861a96aeb5f455e22c6e0c7e
MaryR-code <m.romanovskaia@gmail.com>
2021-06-07 11:40:07 +0300

    #10 changed PUP-method name, access control through @RollesAllowed

--- src/main/java/guru/bug/courses/srs/boundary/RoleResource.java
+++ src/main/java/guru/bug/courses/srs/boundary/RoleResource.java
@@ -6,14 +6,11 @@ import io.smallrye.common.constraint.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
-import java.util.ArrayList;
 import java.util.List;
 
 @Path("/roles")
@@ -24,29 +21,21 @@ public class RoleResource {
     RoleControl roleControl;
 
     @POST
-    @PermitAll
+    @RolesAllowed({"admin"})
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public RoleEntity setRole(@Valid @NotNull RoleEntity role,
-                              @Context SecurityContext sec) {
-        var roleObj = new RoleEntity();
-        if (sec.isUserInRole("admin")) {
-            roleObj = roleControl.createRole(role);
-            LOG.info("Creating new role...");
-        }
-        return roleObj;
+    public RoleEntity createRole(@Valid @NotNull RoleEntity role) {
+        LOG.info("Creating new role with name '{}'", role.getName());
+        return roleControl.createRole(role);
     }
 
     @GET
-    @PermitAll
+    @RolesAllowed({"admin"})
     @Consumes(MediaType.APPLICATION_JSON)
-    public List<RoleEntity> getRoles(@Context SecurityContext sec) {
-        List<RoleEntity> roleList = new ArrayList<>();
-        if (sec.isUserInRole("admin")) {
-            roleList = roleControl.findAll();
-            LOG.info("Selecting list of roles...");
-        }
-        return roleList;
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<RoleEntity> getRoles() {
+        LOG.debug("Selecting list of roles...");
+        return roleControl.findAll();
     }
 
 }
